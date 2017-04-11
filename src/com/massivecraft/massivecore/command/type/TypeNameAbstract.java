c7e3d3dffabfd029d02e3ba69c1628eb63dac743
ulumulu1510 <b.heusser@gmx.ch>
2017-04-11 09:58:26 +0200

    Modify TypeNameAbstract to be broader usable.

--- src/com/massivecraft/massivecore/command/type/TypeNameAbstract.java
+++ src/com/massivecraft/massivecore/command/type/TypeNameAbstract.java
@@ -45,10 +45,10 @@ public abstract class TypeNameAbstract extends TypeAbstract<String>
 		if (arg == null) throw new NullPointerException("arg");
 
 		// Allow changing capitalization of the current name if lenient.
-		String current = this.getCurrentName(sender);
+		String current = this.getCurrentName(sender, arg);
 		if (current != null && current.equalsIgnoreCase(arg) && this.isLenient()) return arg;
 
-		if (this.isNameTaken(arg)) throw new MassiveException().addMsg("<b>The name \"<h>%s<b>\" is already in use.",arg);
+		if (this.isNameTaken(sender, arg)) throw new MassiveException().addMsg("<b>The name \"<h>%s<b>\" is already in use.",arg);
 		
 		Integer lengthMin = this.getLengthMin();
 		if (lengthMin != null && arg.length() < lengthMin)
@@ -71,14 +71,13 @@ public abstract class TypeNameAbstract extends TypeAbstract<String>
 		return Collections.emptyList();
 	}
 	
-	
 	// -------------------------------------------- //
 	// METHODS
 	// -------------------------------------------- //
 	
-	public String getCurrentName(CommandSender sender)
+	public String getCurrentName(CommandSender sender, String arg) throws MassiveException
 	{
-		Named named = this.getCurrent(sender);
+		Named named = this.getCurrent(sender, arg);
 		if (named == null) return null;
 		return named.getName();
 	}
@@ -87,8 +86,8 @@ public abstract class TypeNameAbstract extends TypeAbstract<String>
 	// ABSTRACT
 	// -------------------------------------------- //
 	
-	public abstract Named getCurrent(CommandSender sender);
+	public abstract Named getCurrent(CommandSender sender, String arg) throws MassiveException;
 	
-	public abstract boolean isNameTaken(String name);
+	public abstract boolean isNameTaken(CommandSender sender, String name) throws MassiveException;
 	
 }
